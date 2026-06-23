5ec524355f4509189b067836aa7ad41227e29cc7
Roman <realasdfga@gmail.com>
2026-06-23 03:12:11 +0200

    Register all models in admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from tasks.models import Worker, Position, TaskType, Task
+
+admin.site.unregister(Group)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": ("first_name", "last_name", "position",),
+            },
+        ),
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "task_type", "priority", "deadline", "is_completed",
+    )
+    search_fields = ("name",)
+    list_filter = (
+        "assignees", "task_type", "deadline", "is_completed", "priority",
+    )
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
