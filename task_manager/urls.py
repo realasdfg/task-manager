bc19a53973c8d851a668cb962a2fdb9da0e9f453
Roman <realasdfga@gmail.com>
2026-06-24 02:32:24 +0200

    Create Home Page with numbers of each db entity

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("tasks.urls", namespace="tasks")),
 ] + debug_toolbar_urls()
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Project, Team, Worker, Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_projects = Project.objects.count()
+    num_teams = Team.objects.count()
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    context = {
+        "num_projects": num_projects,
+        "num_teams": num_teams,
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+    }
+    return render(request, "tasks/index.html", context=context)
