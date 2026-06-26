7275fd2d4527d01c6ae47bf2be07938077b79fbb
Roman <realasdfga@gmail.com>
2026-06-27 01:33:11 +0200

    Add new templates design for index, navigation and footer

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "tasks.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -129,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+ASSETS_ROOT = "/static/assets"
 
 AUTH_USER_MODEL = "tasks.Worker"
 
