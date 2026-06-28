0d0b6f71a5e39d0ffe52e84642b41240059d4ef5
Roman <realasdfga@gmail.com>
2026-06-29 01:09:04 +0200

    Add SortMixin to ProjectListView

--- tasks/views/project.py
+++ tasks/views/project.py
@@ -5,18 +5,34 @@ from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views import generic
 
-from task_manager.mixins import SearchMixin, AddObjectNameMixin
+from task_manager.mixins import SearchMixin, AddObjectNameMixin, SortMixin
 from tasks.forms import ProjectForm, ProjectCompleteForm
 from tasks.mixins import PaginationMixin
 from tasks.models import Project
 
 
-class ProjectListView(SearchMixin, LoginRequiredMixin, generic.ListView):
+class ProjectListView(
+    SortMixin,
+    SearchMixin,
+    LoginRequiredMixin,
+    generic.ListView
+):
     model = Project
     queryset = Project.objects.all().prefetch_related("teams")
     search_fields = {"name": "Search by name",
                      "description": "Search by description"}
     paginate_by = 10
+    sort_options = {
+        "name": "Name (A→Z)",
+        "-name": "Name (Z→A)",
+        "deadline": "Deadline (earliest)",
+        "-deadline": "Deadline (latest)",
+        "created_at": "Creation (earliest)",
+        "-created_at": "Creation (latest)",
+        "-is_completed": "Completed first",
+        "is_completed": "Uncompleted first",
+    }
+    default_sort = "-created_at"
 
 
 class ProjectDetailView(
