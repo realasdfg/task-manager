b64fc36a4e6a3f1a369f7ea9458f354b7b030da0
Roman <realasdfga@gmail.com>
2026-06-29 20:03:35 +0200

    Fix missed `/` at the end of teams urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -68,12 +68,12 @@ urlpatterns = [
     path("teams/<int:pk>/", TeamDetailView.as_view(), name="team-detail"),
     path("teams/create/", TeamCreateView.as_view(), name="team-create"),
     path(
-        "teams/<int:pk>/update",
+        "teams/<int:pk>/update/",
         TeamUpdateView.as_view(),
         name="team-update"
     ),
     path(
-        "teams/<int:pk>/delete",
+        "teams/<int:pk>/delete/",
         TeamDeleteView.as_view(),
         name="team-delete"
     ),
